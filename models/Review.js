2457703113da0f77232cab7a041590bfd4d28069
kylemoely <kylemoely@gmail.com>
2023-05-15 14:02:37 -0500

    Set up User and Review models

---
+++ models/Review.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelName: 'review'
+    }
+);
+
+module.exports = { Review };
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            primaryKey:true,
+            validate: {
+                isAlphanumeric:true,
+                len: [5,16],
+            }
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate: {
+                len:[6,20],
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored:false,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
