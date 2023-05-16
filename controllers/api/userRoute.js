a58b9d765c54b18da2ef8e1c7c77f0c73662786b
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-16 10:21:39 -0400

    Added the login handlebars

---
+++ controllers/api/userRoute.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { emitWarning } = require("process");
+const {User} = require("../../models");
+
+router.get("/login", (req, res) => {
+    console.log("hELLO FROM THE USER LOGIN");
+
+
+
+})
+
+router.post("/login", (req, res) => {
+    
+})
+
+module.exports = router;
