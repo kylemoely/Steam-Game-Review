2e10559895453ef60f1d44a07ed96561a4cbcb4e
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-16 10:05:43 -0400

    Added middleware to controllers index.js

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+const userForm = document.getElementById("userForm");
+
+const formSubmission = (event) => {
+    event.preventDefault();
+
+    const userEl = document.getElementById("userNameInput").value;
+    const passwordEl = document.getElementById("passwordInput").value;
+
+
+    if (userEl && passwordEl) {
+        
+    }
+
+
+
+
+}
