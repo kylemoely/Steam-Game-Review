64d6ca7bc9e97aac22a4617e7469bc7eaf2ef1b8
Sammy <sammyhyi@gmail.com>
2023-05-16 15:54:10 -0400

    all seed data shows in get request

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const { Review } = require('../../models');
-const { User } = require('../../models')
+const { Review, User } = require('../../models');
 
 router.get('/:title', async (req, res) => {
-  res.send('YURRRRR');
-})
+  try {
+    const reviewData = await Review.findAll();
 
-// router.post('/:title', async (req, res) => {
-//   try {
-//     const reviewData = await Review.findAll
-//   }
-// });
+    res.status(200).json(reviewData);
+    console.log(reviewData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
