4f1c7d36cbace74a015235f544b9b0a95a3791f7
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-05-16 13:55:53 -0400

    working server

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const {User} = require("../../models");
 
-router.get('/:user', (req, res) => {
+router.get('/', (req, res) => {
     console.log('Welcome User');
-
+res.render('users')
 })
 
-router.post('/:user', (req,res) => {
+router.post('/', (req,res) => {
     console.log('Welcome User');
 
 })
