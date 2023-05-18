7214a86a8328e0142795865200e2c698c28fcb00
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-05-17 21:42:54 -0400

    dont know

--- userpseudoNotes.js
+++ userpseudoNotes.js
@@ -1,26 +1,26 @@
-//To do list 
-if user is signed in display my page, if not signed in throw an error saying not logged in then prompt to sign in)
-User will be displayed their User page.
-User page consist of 3 containers: Prompt container, User Reviews container, a new Post container
+// //To do list 
+// if user is signed in display my page, if not signed in throw an error saying not logged in then prompt to sign in)
+// User will be displayed their User page.
+// User page consist of 3 containers: Prompt container, User Reviews container, a new Post container
 
-Prompt container:
+// Prompt container:
 
-Asks user if they would like to (1)post a review or(2)read other users posted reviews from our website.
-(1) and (2) will be buttons. 
-x(1) will lead to a form to fill out your new post.x
-(2) will lead to the reviews page of our website.
-Dont have time to make a pop out with a form to fill in review after clicking post a review.
-Will instead just have a post section always displayed on user page. 
-
-
-New Review Post container: 
-
-Will be a form. Form will have game title and review section. 
-xForm does not have username section because it will be attatched.x
-Form will have a username section, will display username in section and can not be altered.. will be auto filled. 
+// Asks user if they would like to (1)post a review or(2)read other users posted reviews from our website.
+// (1) and (2) will be buttons. 
+// x(1) will lead to a form to fill out your new post.x
+// (2) will lead to the reviews page of our website.
+// Dont have time to make a pop out with a form to fill in review after clicking post a review.
+// Will instead just have a post section always displayed on user page. 
 
 
-Old User Reviews container:
+// New Review Post container: 
 
-This section will display the Users previous posted reviews.
-displays the game title, name of the reviewer, and the text review. and a photo of the reviewed game. 
+// Will be a form. Form will have game title and review section. 
+// xForm does not have username section because it will be attatched.x
+// Form will have a username section, will display username in section and can not be altered.. will be auto filled. 
+
+
+// Old User Reviews container:
+
+// This section will display the Users previous posted reviews.
+// displays the game title, name of the reviewer, and the text review. and a photo of the reviewed game. 
