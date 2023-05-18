ee6339f772c9e950c42fbe2aef7fe9052ac4239f
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-05-18 13:38:05 -0400

    uncommented some lines

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
-// const homeRoutes = require('./homepageRoute');
+const homeRoutes = require('./homepageRoute');
 const loginRoutes = require('./loginRoute');
-// const resultRoutes = require('./resultRoutes');
-// const reviewRoutes = require('./reviewRoutes');
+const resultRoutes = require('./resultRoutes');
+const reviewRoutes = require('./reviewRoutes');
 const userRoutes = require('./userRoutes');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 
 router.use('/login', loginRoutes);
 
-// router.use('/reviews', reviewRoutes);
+router.use('/reviews', reviewRoutes);
 
-// router.use('/results', resultRoutes);
+router.use('/results', resultRoutes);
 
 router.use('/users', userRoutes);
 
