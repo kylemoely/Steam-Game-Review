414c38456686cf9d97789eec83b71e62094b7d87
christian6404 <christian6404@gmail.com>
2023-05-18 11:41:57 -0400

    finishing code for merge

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
-// const homeRoutes = require('./homepageRoute');
-// const loginRoutes = require('./loginRoute');
+const homeRoutes = require('./homepageRoute');
+const loginRoutes = require('./loginRoute');
 const resultRoutes = require('./resultRoutes');
-// const reviewRoutes = require('./reviewRoutes');
-// const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./reviewRoutes');
+const userRoutes = require('./userRoutes');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 
-// router.use('/login', loginRoutes);
+router.use('/login', loginRoutes);
 
-// router.use('/reviews', reviewRoutes);
+router.use('/reviews', reviewRoutes);
 
 router.use('/results', resultRoutes);
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
 
